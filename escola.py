c5b65289121d67ed2b9c4366015741a6d713c255
Camila Silva <camila.a.silva19@aluno.senai.br>
2026-03-06 17:19:58 -0300

    Testes prontos e aprovados

--- escola.py
+++ escola.py
@@ -1,12 +1,25 @@
 def verificador_media(media:int|float) -> str:
-    if media >= 7:
+
+    # Testando para ver se a média é um número
+    if not isinstance(media,(int,float)):
+        raise TypeError("Tipo inválido, a entrada deve ser numérica")
+    # Varificar se é menor que 0
+    elif media < 0:
+        raise ValueError ("O valor deve ser maior ou igual a 0 e menor ou igual a 10")
+    # Varifica se é maior que 10
+    elif media > 10:
+        raise ValueError ("O valor deve ser maior ou igual a 0, e menor ou igual a 10")
+    # Varifica se a média é maior ou igual a 7 para ser "Aprovado"
+    elif media >= 7:
         return("Aprovado")
-    
-    elif media < 5:
+    # Verifica se a média é menor ou igual a 4 para ser "Reprovado"
+    elif media <= 4:
         return("Reprovado")
-    
-    elif media < 7 and media >= 5:
+    # Verifica se a média é maior ou igual a 5 e menor ou igual a 6 para ficar de "Recuperação"
+    else:
         return("Recuperação")
+    
+    
     
 if __name__ == "__main__":
     print(verificador_media(6))
